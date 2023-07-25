0c5425016f4016c0b2fbd71500848cfbc0667f71
omrajsharma <omraj7413@gmail.com>
2023-07-25 08:57:11 +0530

    feat: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import RegisterPage from './pages/RegisterPage'
--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="header-container">
+        <div className="header-left">
+          <Link to="/" className="header-left-brand">
+            <img src={Logo} alt="" />
+            <h2>Awaas Vishwa</h2>
+          </Link>
+        </div>
+        <div className="header-right">
+          <div className="header-right-login">
+            <Link to="/login">Login</Link>
+            <Link to="/register">register</Link>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
