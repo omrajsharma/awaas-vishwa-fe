84e144c7976d817a9a14ffc13cc2986390fff52f
omrajsharma <omraj7413@gmail.com>
2023-07-26 07:29:29 +0530

    feat: footer section

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <div className="footer-container">
+        <p className="footer-text">
+          Made with ❤️ by 
+          <a href="https://omrajsharma.github.io" target='_blank'><u><i> Omraj Sharma</i></u></a>
+           in 🇮🇳
+        </p>
+      </div>
+    </footer>
   )
 }
 
